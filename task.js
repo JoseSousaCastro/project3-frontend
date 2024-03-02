91f8628dbb0910d6a1a32597943754520fd0b39f
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-03-02 12:34:02 +0000

    update tasks

--- task.js
+++ task.js
@@ -14,7 +14,6 @@ window.onload = async function () {
     try {
       getFirstName(usernameValue, passwordValue);
       getPhotoUrl(usernameValue, passwordValue);
-      loadTasks();
     } catch (error) {
       console.error("An error occurred:", error);
     }
@@ -22,11 +21,6 @@ window.onload = async function () {
 };
 
 const taskId = sessionStorage.getItem("taskId");
-
-// Definir os botões de status
-const todoButton = document.getElementById("todo-button"); // Atribuir o elemento respetivo à variável todoButton
-const doingButton = document.getElementById("doing-button"); // Atribuir o elemento respetivo à variável doingButton
-const doneButton = document.getElementById("done-button"); // Atribuir o elemento respetivo à variável doneButton
 
 // Definir os botões de priority
 const lowButton = document.getElementById("low-button");
@@ -36,23 +30,20 @@ const highButton = document.getElementById("high-button");
 async function updateTask() {
 
   const priority = returnPriorityFromSelectedButton();
-  const stateId = returnStateIdFromSelectedButton();
 
   const task = {
-    id: taskId,
-    title: document.getElementById("title-task").value,
-    description: document.getElementById("description-task").value,
-    category: document.getElementById("category-task").value,
+    id: sessionStorage.getItem("taskId"),
+    title: document.getElementById("titulo-task").value,
+    description: document.getElementById("descricao-task").value,
+    startDate: document.getElementById("startDate-editTask").value,
+    endDate: document.getElementById("endDate-editTask").value,
     priority: priority,
-    stateId: stateId,
-    description: document.getElementById("startDate-editTask").value,
-    category: document.getElementById("endDate-editTask").value,
-         
+    category: document.getElementById("categoria-task").value,
   };
-  let firstNameRequest = `http://localhost:8080/project3-backend/rest/tasks/update`;
+  let updateSelectedTask = `http://localhost:8080/project3-backend/rest/tasks/update`;
   try {
     const response = await fetch(
-      firstNameRequest,
+      updateSelectedTask,
       {
         method: "PUT",
         headers: {
@@ -64,11 +55,11 @@ async function updateTask() {
       }
     );
     const message = await response.text();
-    alert(message);
+    return message; // Return the message received from the backend
   } catch (error) {
     console.error("An error occurred:", error);
     alert("Something went wrong");
-    throw error; // Propagar o erro para ser tratado no catch do bloco que chamou a função
+    throw error;
   }
 }
 
@@ -126,44 +117,23 @@ async function getPhotoUrl(usernameValue, passwordValue) {
   }
 }
 
-async function getAllTasks() {
-  let getTasks = `http://localhost:8080/project3-backend/rest/tasks/all`;
-
-  try {
-    const response = await fetch(getTasks, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/JSON",
-        Accept: "*/*",
-        token: sessionStorage.getItem("token"),
-      },
-    });
-    const message = await response.text();
-    alert(message);
-  } catch (error) {
-    alert("Something went wrong");
-  }
-}
-
 async function showTask(taskId) {
   const task = await findTaskById(taskId);
   if (task) {
-    document.getElementById("title-task").textContent = task.title; // Colocar o título no input title
-    document.getElementById("description-task").textContent = task.description; // Colocar a descrição na text area
-    document.getElementById("category-task").textContent = task.category; // Colocar a descrição na text area
     document.getElementById("tasktitle").innerHTML = task.title; // Colocar o título no título da página
+    document.getElementById("titulo-task").textContent = task.title; // Colocar o título no input title
+    document.getElementById("descricao-task").textContent = task.description; // Colocar a descrição na text area
+    document.getElementById("categoria-task").textContent = task.category; // Colocar a descrição na text area
     document.getElementById("startDate-editTask").value = task.startDate;
-    document.getElementById("endDate-editTask").value = task.limitDate;
+    document.getElementById("endDate-editTask").value = task.endDate;
 
-    let taskPriority = task.priority;
-
-    if (taskPriority == "LOW_PRIORITY") {
+    if (task.priority == "LOW_PRIORITY") {
       lowButton.classList.add("selected");
       setPriorityButtonSelected(lowButton);
-    } else if (taskPriority == "MEDIUM_PRIORITY") {
+    } else if (task.priority == "MEDIUM_PRIORITY") {
       mediumButton.classList.add("selected");
       setPriorityButtonSelected(mediumButton);
-    } else if (taskPriority == "HIGH_PRIORITY") {
+    } else if (task.priority == "HIGH_PRIORITY") {
       highButton.classList.add("selected");
       setPriorityButtonSelected(highButton);
     }
@@ -175,9 +145,17 @@ async function showTask(taskId) {
 }
 
 // Event listeners para os botões priority
-lowButton.addEventListener("click", () => setPriorityButtonSelected(lowButton));
-mediumButton.addEventListener("click", () => setPriorityButtonSelected(mediumButton));
-highButton.addEventListener("click", () => setPriorityButtonSelected(highButton));
+lowButton.addEventListener("click", () => {
+  setPriorityButtonSelected(lowButton);
+});
+
+mediumButton.addEventListener("click", () => {
+  setPriorityButtonSelected(mediumButton);
+});
+
+highButton.addEventListener("click", () => {
+  setPriorityButtonSelected(highButton);
+});
 
 const cancelbutton = document.getElementById("cancel-button");
 
@@ -194,19 +172,11 @@ cancelbutton.addEventListener("click", () => {
   // Event listener para o botão de confirmação
   const confirmButton = document.getElementById("confirm-cancel-button");
   confirmButton.addEventListener("click", () => {
-    sessionStorage.clear();
     window.location.href = "home.html";
   });
 
   cancelModal.style.display = "grid";
 });
-
-// Função para definir o estado no grupo de botões status
-function setStatusButtonSelected(button) {
-  const buttons = [todoButton, doingButton, doneButton];
-  buttons.forEach((btn) => btn.classList.remove("selected"));
-  button.classList.add("selected");
-}
 
 // Função para definir o estado no grupo de botões priority
 function setPriorityButtonSelected(button) {
@@ -267,30 +237,25 @@ function returnPriorityFromSelectedButton() {
   return priorityInt;
 } 
 
-function returnStateIdFromSelectedButton() {
-  const buttons = [todoButton, doingButton, doneButton];
-  let selectedButton = null;
-  buttons.forEach((btn) => {
-    if (btn.classList.contains("selected")) {
-      selectedButton = btn;
-    }
-  });
-  const stateIdInt = selectedButton.innerText.toUpperCase();
-  return stateIdInt;
-}
-
-// Event listener para o botão save
+// Event listener for the save button
 const savebutton = document.getElementById("save-button");
 savebutton.addEventListener("click", async () => {
-
   try {
-      await updateTask();
-      alert("Task updated successfully");
-      sessionStorage.clear();
+    const message = await updateTask(); // Capture the message returned from updateTask()
+
+    // Check if the update task was successful 
+    if (message === "Task updated successfully") {
+      // If successful, clear taskId and navigate to home.html
+      sessionStorage.setItem("taskId", "");
+      alert(message);
       window.location.href = "home.html";
-   
+    } else {
+      // If not successful, display an alert message
+      alert(message);
+    }
   } catch (error) {
     console.error("Error:", error);
     alert("Something went wrong while updating the task");
   }
 });
+
